adb3a1ddbae65bb33d74c160aa13872676bb6ce3
Anubhajain05 <39409413+Anubhajain05@users.noreply.github.com>
2020-08-04 16:52:44 +0530

    Add files via upload

---
+++ mysite/about/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from about.models import Booking
+from about.models import Hotel_staff
+
+admin.site.register(Booking)
+admin.site.register(Hotel_staff)
+
+# Register your models here.
